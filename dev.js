c0ca67c2de5d9304eedaf80e9fe7ecfd6b94e8da
RecDecLec <recdeclec.gamemkr@gmail.com>
2016-06-06 18:33:32 -0400

    Update dev.js

--- dev.js
+++ dev.js
@@ -3,5 +3,5 @@ function editbyid(id, text) {
     document.getElementById(id).innerHTML = text;
  }
  function editbyid2(id, text) {
-    document.getElementById("id").innerHTML = "text";
+    document.getElementById("id").innerHTML = text;
  }
