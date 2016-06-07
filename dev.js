32ec25fb855b7b432430a9d809f56a1e91340811
RecDecLec <recdeclec.gamemkr@gmail.com>
2016-06-07 11:25:30 -0400

    Update dev.js

--- dev.js
+++ dev.js
@@ -1,3 +1,4 @@
+var score = 0;
 function edittext(id, text) {
     document.getElementById(id).innerHTML = text;
  }
@@ -7,4 +8,10 @@ function edittext(id, text) {
     para.appendChild(node);
     para.id=Math.floor(Math.random() * 11);
  }
- 
+ function scorechange(value) {
+     var score = score + value;
+     updatescore();
+ }
+function updatescore(){
+    document.getElementById("score").innerHTML = score;
+}
